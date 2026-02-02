a39f4204cccc74baf59cad414d75bc4ad52a96d1
betingrich4 <mariseltru@gmail.com>
2026-02-02 09:55:15 +0300

    Create config.js

---
+++ config.js
@@ -0,0 +1,7 @@
+require('dotenv').config();
+global.sessionid = process.env.SESSION_ID || '';
+global.BOT_PREFIX = '.';
+global.owners = [
+    '254740007567@lid',
+    '25474000756@s.whatsapp.net'
+];
